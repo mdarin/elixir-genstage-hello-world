e1155c19cbf377c8e6c14fa936ade1a898f7ccab
mdariin <M.Dariin@skoltech.ru>
2019-10-24 17:25:11 +0300

    unpdate moduledoc

--- lib/genstage_ex/application.ex
+++ lib/genstage_ex/application.ex
@@ -1,12 +1,16 @@
 defmodule GenstageEx.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
+
   @moduledoc """
-	  For our application we’ll use all three GenStage roles. 
-		Our producer will be responsible for counting and emitting numbers.
-		We’ll use a producer-consumer to filter out only the even numbers 
-		and later respond to demand from downstream. Last we’ll build a 
-		consumer to display the remaining numbers for us.
+	For our application we’ll use all three GenStage roles.
+	Our producer will be responsible for counting and emitting numbers.
+	We’ll use a producer-consumer to filter out only the even numbers
+	and later respond to demand from downstream. Last we’ll build a
+	consumer to display the remaining numbers for us.
+
+	Usage:
+		mix run --no-halt
 	""" 
 
   use Application
